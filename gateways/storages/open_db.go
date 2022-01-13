45b431df06df8dbe901ffbcd0b294a975b14c434
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-01-13 21:53:33 +0300

    Add the auto-migration

--- gateways/storages/open_db.go
+++ gateways/storages/open_db.go
@@ -3,6 +3,7 @@ package storages
 import (
 	"fmt"
 
+	"github.com/svetlana-rezvaya/go-recipes-backend/entities"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,6 +13,10 @@ func OpenDB(dsn string) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("unable to open the DB: %w", err)
+	}
+
+	if err := db.AutoMigrate(&entities.Ingredient{}); err != nil {
+		return nil, fmt.Errorf("unable to auto-migrate: %w", err)
 	}
 
 	return db, nil
